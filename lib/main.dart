de14cd48ad9abdbf6f2f51209226413a26804c4e
SASCHADDD <saschadanu23@gmail.com>
2026-06-17 22:59:42 +0700

    Refactor main.dart to implement BlocProvider for AuthBloc and integrate necessary dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flextime_mobile/services/notification_service.dart';
-import 'ui/pages/auth/login_page.dart';
+import 'package:flextime_mobile/data/providers/api_provider.dart';
+import 'package:flextime_mobile/data/providers/storage_provider.dart';
+import 'package:flextime_mobile/data/repositories/auth/auth_repository.dart';
+import 'package:flextime_mobile/logic/bloc/auth/auth_bloc.dart';
+import 'ui/pages/splash/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,13 +18,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlexTime',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00ACC1)),
+    return BlocProvider(
+      create: (_) => AuthBloc(
+        authRepository: AuthRepository(
+          apiProvider: ApiProvider(),
+          storageProvider: StorageProvider(),
+        ),
       ),
-      home: const LoginPage(),
+      child: MaterialApp(
+        title: 'FlexTime',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00ACC1)),
+        ),
+        home: const SplashPage(),
+      ),
     );
   }
 }
